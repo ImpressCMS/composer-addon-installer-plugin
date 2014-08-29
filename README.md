9593fa625226e064c363f092bdc22dd04de8ad7e
David Janssens <fiammybe@gmail.com>
2014-08-29 18:12:06 +0200

    Composer plugin to install ImpressCMS modules - first version

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 composer-module-installer-plugin
 ================================
 
-a plugin for Composer to allow the installation of ImpressCMS modules
+a plugin for Composer to allow the installation of ImpressCMS modules. Instead of forcing the
+installation in the /vendor folder, these modules will be installed in the /modules folder.
---
+++ src/ModuleInstaller.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fiammy
+ * Date: 29/08/14
+ *
+ *
+ */
+
+namespace ImpressCMS\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ModuleInstaller extends LibraryInstaller
+{
+    /**
+     * getPackageBasePath
+     *
+     * @param PackageInterface $package package being installed
+     *
+     * @return string install path relative to composer.json
+     */
+    public function getPackageBasePath(PackageInterface $package)
+    {
+        $moddir = explode('/', $package->getName());
+        $icms_modules = '../modules/';
+        $extra = $this->composer->getPackage()->getExtra();
+        if (isset($extra['icms_modules_path'])) {
+            $icms_root = $extra['icmss_modules_path'];
+        }
+        return $icms_modules . $moddir[1];
+    }
+    /**
+     * supports - determine if this supports a given package type
+     *
+     * @param string $packageType package type name
+     *
+     * @return boolean true if packageType is supported
+     */
+    public function supports($packageType)
+    {
+        return 'icms-module' === $packageType;
+    }
+} 
